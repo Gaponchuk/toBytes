140321f653bc6a4af6cd2e16ac4e77d0183d54dd
Yuriy <gaponchykyn@gmail.com>
2018-01-25 01:30:32 +0500

    Add files via upload

--- js/script.js
+++ js/script.js
@@ -5,14 +5,16 @@ var closeMenu = document.getElementById("close-menu");
 
 openMenu.addEventListener("click", function(){
 	nav.style.transform = "translateY(0)";
+	nav.classList.add("active");
+
+	for(var i = 0; i < navLinks.length; i++) {
+		navLinks[i].addEventListener("click", function(){
+			nav.style.transform = "translateY(-100vh)";
+		})
+	}
 });
 
 closeMenu.addEventListener("click", function(){
 	nav.style.transform = "translateY(-100vh)";
 });
 
-for(var i = 0; i < navLinks.length; i++) {
-	navLinks[i].addEventListener("click", function(){
-		nav.style.transform = "translateY(-100vh)";
-	})
-}
