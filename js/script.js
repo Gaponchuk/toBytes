533dc042c76056cd2e690889e176628e83866835
Yuriy <gaponchykyn@gmail.com>
2018-01-24 09:29:13 +0500

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,4 @@
+var navLinks = document.querySelectorAll(".navigation__link");
+var burgerMenu = document.tagName(".btn-menu__item");
+var nav = document.getElementById("navigation");
+var btnMenu = document.querySelector(".btn-menu");
